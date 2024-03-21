02eed6db79544f1c0b8f7210be52ce35e97082d6
Justin Goh <justingoh@r-217-107-25-172.comp.nus.edu.sg>
2024-03-21 18:51:19 +0800

    reset bass css and settled login

---
+++ poptrade/src/uifire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCr87UQwx_ErNFKGiCD7s9zT5tvmPwinq0",
+    authDomain: "poptrade-c2650.firebaseapp.com",
+    projectId: "poptrade-c2650",
+    storageBucket: "poptrade-c2650.appspot.com",
+    messagingSenderId: "327732712069",
+    appId: "1:327732712069:web:011bc4cfb211fafcd8cfec",
+    measurementId: "G-E2H66S2NVC"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
