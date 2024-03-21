b223907926f18e386522da5986c49f5c9d3f68dc
Justin Goh <justingoh@r-217-107-25-172.comp.nus.edu.sg>
2024-03-21 16:40:53 +0800

    deleted the other stuff

--- poptrade/src/views/HomeView.vue
+++ poptrade/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+    <div class="home-view">
+
+    </div>
+  </template>
+  
+  <script>
+  // Import the Navbar component
+  import Navbar from './components/Navbar.vue';
+  
+  export default {
+    name: 'HomeView',
+    components: {
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles specific to HomeView if necessary */
+  .home-view {
+    /* Your styles here */
+  }
+  </style>
+  
