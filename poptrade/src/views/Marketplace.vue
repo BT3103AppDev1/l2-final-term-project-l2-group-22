b66a3b0080b3eb8e6611b32fae2fdc60e2234ea4
Priscilia <e0968980@u.nus.edu>
2024-03-22 15:34:27 +0800

    Added button to ViewListing in Marketplace

--- poptrade/src/views/Marketplace.vue
+++ poptrade/src/views/Marketplace.vue
@@ -1,11 +1,11 @@
 <template>
-
 	<div class="marketplace">
 		<h1>Marketplace</h1>
 		<!-- Placeholder content for Marketplace -->
 		<p>This is the marketplace page.</p>
 		<!-- just to check what ViewListing -->
-		<ViewListing />
+		<button @click="goToViewListing">Go to View Listing</button>
+		<!-- <ViewListing /> -->
 	</div>
 </template>
 
@@ -17,10 +17,15 @@ export default {
 		ViewListing,
 	},
 	// You might include component logic here later
+	methods: {
+		goToViewListing() {
+			// Programmatic navigation to ViewListing.vue
+			this.$router.push({ name: "ViewListing" }); // Assuming 'ViewListing' is the name of the route for ViewListing.vue
+		},
+	},
 };
 </script>
 
 <style scoped>
 /* Marketplace specific styles */
 </style>
-
